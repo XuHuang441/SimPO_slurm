33cc4be36cf13f21c88e08a40aff7e7901b36e7f
huangxu <xhuang441@qq.com>
2025-08-18 11:11:12 +0800

    debug: iter2 train 6gpu
    training collapsed

--- test_dataset.py
+++ test_dataset.py
@@ -3,4 +3,4 @@ from datasets import load_from_disk
 dataset = load_from_disk("/hai/scratch/fangwu97/xu/SimPO_slurm/data/inpo_iter2/pref/")
 print(dataset)
 print(dataset.column_names)
-print(dataset[0])  # 查看第一条数据
+print(dataset['train'][0])  # 查看第一条数据
