25cf428fb53a3592bf90cf6fcab86c4ea4ebf8c3
huangxu <xhuang441@qq.com>
2025-08-18 11:00:53 +0800

    debug: iter2 train 6gpu
    training collapsed

--- upload_hf_dataset.py
+++ upload_hf_dataset.py
@@ -2,7 +2,7 @@ from huggingface_hub import HfApi
 
 api = HfApi()
 api.upload_folder(
-    folder_path="/hai/scratch/fangwu97/xu/SimPO_slurm/datasets/gemma2_ultrafeedback/inpo_iter2",
+    folder_path="/hai/scratch/fangwu97/xu/SimPO_slurm/data/inpo_iter2/pref/",
     repo_id="XuHuang/inpo_iter1",
     repo_type="dataset"
 )
