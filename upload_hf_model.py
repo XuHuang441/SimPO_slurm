5081033105128e912ed720119f745fef8123304b
huangxu <xhuang441@qq.com>
2025-08-19 17:55:42 +0800

    run: iter3 decode

---
+++ upload_hf_model.py
@@ -0,0 +1,9 @@
+from huggingface_hub import HfApi
+import os
+
+api = HfApi(token=os.getenv("HF_TOKEN"))
+api.upload_folder(
+    folder_path="/hai/scratch/fangwu97/xu/SimPO_slurm/outputs/gemma-2-9b-it_inpo_stage_2/",
+    repo_id="XuHuang/simpo_inpo_iter2_aug_19",
+    repo_type="model",
+)
