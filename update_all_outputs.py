b9c78d7e349f9bb68a6ab50b510740ab8aa358cf
huangxu <xhuang441@qq.com>
2025-08-19 09:46:11 +0800

    debug: iter2 train 6gpu
    training collapsed
    
    regen empty strs

--- update_all_outputs.py
+++ update_all_outputs.py
@@ -4,7 +4,6 @@ from huggingface_hub import hf_hub_download
 file_path = hf_hub_download(
     repo_id="XuHuang/inpo_iter1",
     filename="all_outputs.json",
-    cache_dir="/hai/scratch/fangwu97/xu/SimPO_slurm/datasets/gemma2_ultrafeedback/inpo_iter2/"
 )
 
 print("文件已下载到:", file_path)
