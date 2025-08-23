3ece0030bc3f78b2fe8f6514faf32e46b4ea23cd
huangxu <xhuang441@qq.com>
2025-08-23 21:02:45 +0800

    update: simpo on policy iter3

--- upload_hf_model.py
+++ upload_hf_model.py
@@ -3,7 +3,7 @@ import os
 
 api = HfApi(token=os.getenv("HF_TOKEN"))
 api.upload_folder(
-    folder_path="/hai/scratch/fangwu97/xu/SimPO_slurm/outputs/gemma-2-9b-it_op_tdpo_stage_3/",
-    repo_id="XuHuang/simpo_inpo_op_iter3_aug22",
+    folder_path="/hai/scratch/fangwu97/xu/SimPO_slurm/outputs/gemma-2-9b-it-simpo-iter1_on_policy/",
+    repo_id="XuHuang/simpo_onPolicy_iter1_aug23",
     repo_type="model",
 )
