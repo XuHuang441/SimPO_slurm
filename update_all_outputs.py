68f9ffa6e72984c5415a1a09fa911fdfc6968714
huangxu <xhuang441@qq.com>
2025-08-24 10:03:44 +0800

    update: simpo on policy iter3

--- update_all_outputs.py
+++ update_all_outputs.py
@@ -3,7 +3,7 @@ from huggingface_hub import hf_hub_download
 # 下载指定文件
 file_path = hf_hub_download(
     repo_id="XuHuang/inpo_iter1",
-    filename="all_outputs_iter3.json",
+    filename="all_outputs.json",
 )
 
 print("文件已下载到:", file_path)
