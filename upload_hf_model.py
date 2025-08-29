d5a5377fe8da24db4ac8c6e93bd70c9d261beb58
huangxu <xhuang441@qq.com>
2025-08-29 19:40:30 +0800

    inpo iter3 train

--- upload_hf_model.py
+++ upload_hf_model.py
@@ -3,7 +3,7 @@ import os
 
 api = HfApi(token=os.getenv("HF_TOKEN"))
 api.upload_folder(
-    folder_path="/hai/scratch/fangwu97/xu/SimPO_slurm/outputs/gemma-2-9b-it_inpo_stage_2/",
-    repo_id="XuHuang/inpo_iter2_aug28",
+    folder_path="/hai/scratch/fangwu97/xu/SimPO_slurm/outputs/gemma-2-9b-it_inpo_1_iter/",
+    repo_id="XuHuang/gemma-2-9b-it_DPO",
     repo_type="model",
 )
