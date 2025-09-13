2184d5cb5800937e8c2d468c7e2fbd107080a478
huangxu <xhuang441@qq.com>
2025-09-13 22:51:13 +0800

    sppo

--- test_dataset.py
+++ test_dataset.py
@@ -1,7 +1,7 @@
 from datasets import load_from_disk
 
-# dataset = load_from_disk("/hai/scratch/fangwu97/xu/SimPO_slurm/data/inpo_iter2/pref/")
-dataset = load_from_disk("/hai/scratch/fangwu97/xu/SimPO_slurm/datasets/gemma2_ultrafeedback/inpo_iter2_skywork/all_outputs_bin_dataset")
+dataset = load_from_disk("/hai/scratch/fangwu97/xu/SimPO_slurm/data/sppo_iter1/pref/")
+# dataset = load_from_disk("/hai/scratch/fangwu97/xu/SimPO_slurm/datasets/gemma2_ultrafeedback/inpo_iter2_skywork/all_outputs_bin_dataset")
 print(dataset)
 print(dataset.column_names)
 print(dataset['train'][0])  # 查看第一条数据
